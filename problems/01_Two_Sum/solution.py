c3260cbefa52f461384b14946739052284bda0b4
kiran <ushakiranbudati.503@gmail.com>
2023-11-24 15:07:01 +0530

    folder starture updated

--- problems/01_Two_Sum/solution.py
+++ problems/01_Two_Sum/solution.py
@@ -3,9 +3,9 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for l in range(len(nums)):
-            complement = target - nums[l]
-            if complement in nums[l + 1:]:
-                return [l, nums.index(complement, l + 1)]
+            sub_values = target - nums[l]
+            if sub_values in nums[l + 1:]:
+                return [l, nums.index(sub_values, l + 1)]
 sol = Solution()
 nums = [3, 2, 4]
 target = 6
